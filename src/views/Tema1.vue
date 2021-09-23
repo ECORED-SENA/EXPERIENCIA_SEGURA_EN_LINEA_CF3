431f85e69a48eb3d50fcb574e44541fff97c2e28
luisEfe80 <luismendez80@hotmail.com>
2021-09-23 10:59:46 -0400

    Adicion de contenidos

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -27,12 +27,17 @@
 
     #t_1_1.titulo-segundo
       h2 1.1  Uso prolongado de dispositivos
+    .row.mb-5(data-aos="flip-up")
+      .col-lg-2
+        figure.mb-3(data-aos="flip-left")
+          img.nW(src="@/assets/curso/t1-ic2.svg", alt="icono decorativo")
 
+      .col-lg-10
+        p.mb-5 El uso abusivo y prolongado de dispositivos electrónicos afectan tanto física como mentalmente a las personas, generando depresión, ansiedad, riesgo de suicidio y adicciones; así como fallas en la atención, memoria y aprendizaje. El siguiente recurso ampliará la información con respecto al uso prolongado de los dispositivos:
 
-
-
-
-
+    figure.mb-5
+      .video
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
 
     Separador
@@ -40,13 +45,55 @@
     br
     #t_1_2.titulo-segundo
       h2 1.2  Riesgos y recomendaciones de buen uso
+    .row.mb-5
+      .col-lg-4
+        .animatedImage
+          figure.mb-5
+            img(src="@/assets/curso/t1-i2.png", alt="mujer usando un teléfono celular")
+          .floatingCircle
+          .floatingCircle2
+      .col-lg-8
+        p.mb-4.text-bold.blueT  Es importante conocer los riesgos de utilizar tecnologías digitales, y ser conscientes que hacer frente a ellos es un componente clave para tener una identidad digital. 
+        p.mb-3  Las habilidades digitales no solo consisten en saber manejar un dispositivo a la perfección, sino también tener conciencia de que la seguridad de la información es igual a cuando se transita por un sector que no genera confianza y se toma las medidas necesarias para protegerse. En el mundo digital no es diferente, se debe cuidar que el uso de los dispositivos no genere una adicción digital, ya que en los últimos años ha desencadenado múltiples problemas físicos y mentales; entre los riesgos más comunes están:
 
+    //-- Aqui vienen las imagenes
 
-
-
-
-
-
+    .row.mb-5
+      .col-lg-3
+        figure.mb-5(data-aos="slide-right")
+          img(src="@/assets/curso/t1-i3.svg", alt="imagen decorativa")
+      .col-lg-9(data-aos="slide-left")
+        p.mb-4.text-bold.blueT ¿Sabía que los riesgos ante el uso de los dispositivos podrían ser interminables?
+        p.mb-4  Esto debido a que el uso excesivo de la tecnología se ve sesgado por el autocontrol por la cantidad de información que recibe a diario el cerebro. Por ello, es importante tomar en cuenta algunas consideraciones para evitar problemas físicos y psicológicos que harán que la tecnología esté bajo el control del usuario y no al contrario.
+        .cajon.color-acento-botones.p-4
+          p.mb-0.text-bold  El ser humano es pensante y racional, capaz de discernir cuando es el momento de hacer un alto y no depender de un aparato electrónico.
+    
+    p.mb-5  A continuación, se enlistan las recomendaciones para un buen uso de la tecnología:
+    .row.justify-content-center.mb-5
+      .col-lg-12
+        .wrap-infografic
+          .row.justify-content-center
+            .col-lg-6
+              ImagenInfografica.color-secundario.mb-5
+                template(v-slot:imagen)
+                  figure
+                    img(src='@/assets/curso/t1-info3.svg', alt='recomendaciones')
+                .tarjeta.color-acento-botones.p-3.shadow-box(x="0%" y="30%")
+                  p Realiza actividades de ocio diferentes a las ofrecidas por las tecnologías.
+                .tarjeta.color-acento-botones.p-3.shadow-box(x="30%" y="0%")
+                  p Apaga o silencia tus dispositivos al menos dos horas antes de ir a descansar en la noche.
+                .tarjeta.color-acento-botones.p-3.shadow-box(x="71%" y="0%")
+                  p Controla la información que se observa, evita páginas que no ayudan ni apoyen a tu autoestima.
+                .tarjeta.color-acento-botones.p-3.shadow-box(x="100%" y="30%")
+                  p Accede a contenidos que ayuden al desarrollo cognitivo y emocional.
+                .tarjeta.color-acento-botones.p-3.shadow-box(x="100%" y="71%")
+                  p Fija horarios y estipula prioridades.
+                .tarjeta.color-acento-botones.p-3.shadow-box(x="71%" y="100%")
+                  p Deja a un lado los dispositivos cuando realices actividades como alimentarte, dormir y hacer ejercicio.
+                .tarjeta.color-acento-botones.p-3.shadow-box(x="30%" y="100%")
+                  p Socializa de manera directa y guarda los dispositivos cuando estés en interacción con personas.
+                .tarjeta.color-acento-botones.p-3.shadow-box(x="0%" y="71%")
+                  p Identifica cualquier síntoma de adicción digital o necesidad excesiva del uso de las tecnologías.
 
     Separador
     br
@@ -109,4 +156,14 @@ export default {
 }
 </script>
 
-<style lang="sass" scoped></style>
+<style lang="sass" scoped>
+.floatingCircle, .floatingCircle2
+  background-image: url('../assets/curso/circle1.svg')
+  background-size: cover
+.wrap-infografic
+  background-image: url('../assets/curso/bg-gray.svg')
+  background-repeat: no-repeat
+  background-size: cover
+  background-position: center
+  padding: 4rem 3rem
+</style>
